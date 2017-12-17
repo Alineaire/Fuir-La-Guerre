3917e8ef50e28640dd18dfa87e906a8ffe65378e
Alineaire <contact@alineaire.com>
2017-12-17 04:59:55 +0100

    Some tests
    Signed-off-by: Alineaire <contact@alineaire.com>

---
+++ Assets/Resources/Scenes/JoystickTests/JoystickTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JoystickTest : MonoBehaviour {
+
+	public Text uiText;
+
+	void Update () {
+		uiText.text = "Left : " +
+			(JoystickReceiver.instance.left.IsPressed ? "true" : "false").ToString();
+	}
+}
